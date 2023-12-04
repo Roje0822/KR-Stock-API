ec304099d42636929e237163e24683f2fad87453
Roje <teahyeon2009@naver.com>
2023-12-04 22:19:20 +0900

    ETC

--- src/main/java/xyz/chide1/stockapi/Stock.java
+++ src/main/java/xyz/chide1/stockapi/Stock.java
@@ -1,36 +1,128 @@
 package xyz.chide1.stockapi;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
+import xyz.chide1.stockapi.parser.WebParser;
 
 public class Stock {
 
     private final Document document;
 
     public Stock(String stockCode) {
-        String url = "https://finance.naver.com/item/main.naver?code=";
-        try {
-            this.document = Jsoup.connect(url + stockCode).get();
-        } catch (IOException e) {
-            throw new RuntimeException("주식 불러오기를 실패했습니다");
-        }
+        this.document = WebParser.getHtmlAsString(stockCode);
     }
 
-    public String getCurrentStockPrice() {
-        Elements contentWrap = document.body().getElementsByClass("content_wrap");
-        Element blindElement = contentWrap.get(0).getElementsByClass("spot").get(0).getElementsByClass("blind").get(1);
-        System.out.println(blindElement.text());
-
-        return document.select("content_wrap").val();
+    /**
+     * 현재 주식 가격을 반환합니다.
+     * @return 주식 가격
+     */
+    public int getCurrentStockPrice() {
+        String stockPriceElement = getBlind(3);
+        return Integer.parseInt(stockPriceElement.split(" ")[1].replace(",", ""));
     }
 
-    public static void main(String[] args) {
-        Stock stock = new Stock("004100");
-        System.out.println(stock.getCurrentStockPrice());
+    /**
+     * 전일대비 상승 가격을 반환합니다.
+     * @return 상승 가격
+     */
+    public int getIncreasePrice() {
+        String increasePriceElement = getBlind(3);
+        int increasePrice = Integer.parseInt(increasePriceElement.split(" ")[4].replace(",", ""));
+        if (isIncreasePrice()) return increasePrice;
+        else return increasePrice * -1;
+    }
+
+    /**
+     * 전일대비 상승 퍼센트를 반환합니다.
+     * @return 상승 퍼센트
+     */
+    public float getIncreasePercent() {
+        String increasePercentElement = getBlind(3);
+        float increasePercent = Float.parseFloat(increasePercentElement.split(" ")[6]);
+        if (isIncreasePrice()) return increasePercent;
+        else return increasePercent * -1;
+    }
+
+    /**
+     * 전일가를 반환합니다.
+     * @return 전일가
+     */
+    public int getPreviousStockPrice() {
+        String previousStockPriceElement = getBlind(4);
+        return Integer.parseInt(previousStockPriceElement.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 시가를 반환합니다.
+     * @return 시가
+     */
+    public int getOpeningPrice() {
+        String stockOpeningPriceElement = getBlind(5);
+        return Integer.parseInt(stockOpeningPriceElement.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 고가를 반환합니다.
+     * @return 고가
+     */
+    public int getHigh() {
+        String  stockLowElement = getBlind(6);
+        return Integer.parseInt(stockLowElement.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 저가를 반환합니다.
+     * @return 저가
+     */
+    public int getLow() {
+        String  stockHighElement = getBlind(8);
+        return Integer.parseInt(stockHighElement.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 상한가를 반환합니다.
+     * @return 상한가
+     */
+    public int getUpperLimitPrice() {
+        String stockUpperLimitPrice = getBlind(7);
+        return Integer.parseInt(stockUpperLimitPrice.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 하한가를 반환합니다.
+     * @return 하한가
+     */
+    public int getLowerLimitPrice() {
+        String stockLowerLimitPrice = getBlind(9);
+        return Integer.parseInt(stockLowerLimitPrice.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 거래량을 반환합니다.
+     * @return 거래량
+     */
+    public int getVolume() {
+        String  stockVolume = getBlind(10);
+        return Integer.parseInt(stockVolume.split(" ")[1].replace(",", ""));
+    }
+
+    /**
+     * 거래대금을 반환합니다.
+     * @return 거래대금
+     */
+    public long getTradingValue() {
+        String stockTradingValue = getBlind(11);
+        return Long.parseLong(stockTradingValue.split(" ")[1].replace(",", "").replace("백만", "")) * 1000000;
+    }
+
+    private boolean isIncreasePrice() {
+        String blindInformation = getBlind(3);
+        return blindInformation.contains("상승");
+    }
+
+    private String getBlind(int i) {
+        Elements totalInformation = document.body().getElementsByClass("new_totalinfo");
+        return totalInformation.get(0).getElementsByClass("blind").get(0).getElementsByTag("dd").get(i).text();
     }
 
 }
--- src/main/java/xyz/chide1/stockapi/parser/WebParser.java
+++ src/main/java/xyz/chide1/stockapi/parser/WebParser.java
@@ -2,14 +2,20 @@ package xyz.chide1.stockapi.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class WebParser {
 
-    public static Document getHtmlAsString(String stockCode) throws IOException {
+    public static Document getHtmlAsString(String stockCode) {
         String url = "https://finance.naver.com/item/main.naver?code=" + stockCode;
-        Document document = Jsoup.connect(url).get();
+        Document document;
+        try {
+            document = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new RuntimeException("주식 불러오기를 실패했습니다");
+        }
         return document;
     }
 }
